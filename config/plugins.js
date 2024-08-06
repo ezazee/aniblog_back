d08ee474f655b3d15555d4d9221882176a123909
ezazee <yukiirima123@gmail.com>
2024-08-07 01:50:57 +0700

    nganu

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,25 @@
-module.exports = () => ({});
+// module.exports = ({ env }) => ({
+//   "vercel-deploy": {
+//     enabled: true,
+//     config: {
+//       deployHook:
+//         "https://api.vercel.com/v1/integrations/deploy/prj_aUik4tky2oIgZoj2yZ7hUG579Iuo/kgcwQ1WltO",
+//       apiToken: "lLtBK7g9v6qVMx9RUNIx3B8p",
+//       appFilter: "strapi_animae",
+//       teamFilter: "your-team-id-on-vercel",
+//       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
